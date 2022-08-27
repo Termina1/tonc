979dd75cdc137b2ae64358542a6faf3490b31908
terminal <terminal2010@gmail.com>
2022-08-28 03:41:53 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# TONC (TON Compiler)
+
+Packaged Func compiler for TON smart contracts.
+
+This is a heavy rewrite of [ton-compiler](https://github.com/ton-foundation/ton-compiler) package, so kudos to its authors.
+All commits were erased, because there were heavy binaries in git and we don't want them to stuck in history.
+
+## Features
+
+- 🚀 Automatically downloads binaries from last release for you OS
+- 🍰 Programmatic and CLI interfaces
+- 💸 Ready to use in unit testing
+
+## Install
+
+```bash
+npm install tonc
+```
+
+## How to use
+
+This packages adds multiple binaries: func, fift and ton-compiler.
+
+- Fift compiler already has stdlib included
+- Func compiler needs stdlib to be provided, you can use bundled version: `./node_modules/tonc/funclib/stdblib.fc`
+- Ton Compiler is a wrapper to compile everything in one go
+
+### Console Use
+
+```bash
+# Compile to binary form (for contract creation)
+tonc --input ./wallet.fc --output ./wallet.cell
+
+# Compile to fift (useful for debuging)
+tonc --input ./wallet.fc --output ./wallet.fif --fift
+```
+
+## Programmatic use
+
+```typescript
+import { compileFunc } from "tonc";
+let compiled = await compileFunc("source code");
+console.log(compiled.fift); // Compiled Fift assembler
+console.log(compiled.cell.toString("hex")); // Compiled cell
+```
+
+# License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { file as createTempFile } from "tmp-promise";
+import { promises as fs } from "fs";
+import * as path from "path";
+import * as child from "child_process";
+
+export function executeFunc(args: string[]) {
+  const fiftPath = path.resolve(__dirname, "..", "bin", "exec", "func");
+  child.execSync(fiftPath + " " + args.join(" "), {
+    stdio: "inherit",
+  });
+}
+
+export function executeFift(args: string[]) {
+  const fiftPath = path.resolve(__dirname, "..", "bin", "exec", "fift");
+  child.execSync(fiftPath + " " + args.join(" "), {
+    stdio: "inherit",
+    env: {
+      FIFTPATH: path.resolve(__dirname, "..", "fiftlib"),
+    },
+  });
+}
+
+export async function compileFunc(source: string): Promise<string> {
+  let sourceFile = await createTempFile({ postfix: ".fc" });
+  let fiftFile = await createTempFile({ postfix: ".fif" });
+  let funcLib = path.resolve(__dirname, "..", "funclib", "stdlib.fc");
+  try {
+    await fs.writeFile(sourceFile.path, source);
+    executeFunc(["-PS", "-o", fiftFile.path, funcLib, sourceFile.path]);
+    let fiftContent = await fs.readFile(fiftFile.path);
+    fiftContent = fiftContent.slice(fiftContent.indexOf("\n") + 1); // Remove first line
+    return fiftContent.toString();
+  } finally {
+    sourceFile.cleanup();
+    fiftFile.cleanup();
+  }
+}
+
+export async function compileFift(source: string): Promise<Buffer> {
+  let fiftOpFile = await createTempFile({ postfix: ".fif" });
+  let cellFile = await createTempFile({ postfix: ".cell" });
+  try {
+    let body = "";
+    body += `"Asm.fif" include\n`;
+    body += source;
+    body += "\n";
+    body += `boc>B "${cellFile.path}" B>file`;
+    await fs.writeFile(fiftOpFile.path, body, "utf-8");
+    executeFift([fiftOpFile.path]);
+    return await fs.readFile(cellFile.path);
+  } finally {
+    fiftOpFile.cleanup();
+    cellFile.cleanup();
+  }
+}
